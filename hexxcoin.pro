0006c68c3513f2a5199f76f9205aec82fa2dc8c6
hexxcoin <hex@mail.com>
2015-08-16 01:29:41 +0300

    version/algo cleaning

--- hexxcoin.pro
+++ hexxcoin.pro
@@ -1,6 +1,6 @@
 TEMPLATE = app
 TARGET = hexx-qt
-VERSION = 2.0.0.9
+VERSION = 2.0.3.1
 INCLUDEPATH += src src/json src/qt
 QT += network
 DEFINES += ENABLE_WALLET
--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60030;
+static const int PROTOCOL_VERSION = 60031;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -38,9 +38,9 @@ static const int INIT_PROTO_VERSION = 209;
 // disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 60029;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 60020;
+static const int MIN_INSTANTX_PROTO_VERSION = 60028;
 
-static const int MIN_MN_PROTO_VERSION = 60020;
+static const int MIN_MN_PROTO_VERSION = 60028;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -51,9 +51,9 @@ static const int NOBLKS_VERSION_START = 0;
 static const int NOBLKS_VERSION_END = 60028;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 60028;
 
 // "mempool" command, enhanced "getdata" behavior starts with this version:
-static const int MEMPOOL_GD_VERSION = 60002;
+static const int MEMPOOL_GD_VERSION = 60028;
 
 #endif
